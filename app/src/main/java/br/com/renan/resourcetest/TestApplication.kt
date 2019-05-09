817afd5ef446ae1965da4fc84ed845a79fdf92ae
rodilon <rodilon@brq.com>
2019-05-09 00:48:20 -0300

    Adiciona o calligraphy para utilizar as fontes necessárias e ajustes de layout do login

--- app/src/main/java/br/com/renan/resourcetest/TestApplication.kt
+++ app/src/main/java/br/com/renan/resourcetest/TestApplication.kt
@@ -2,12 +2,25 @@ package br.com.renan.resourcetest
 
 import android.app.Application
 import br.com.renan.resourcetest.provider.NetworkProvider
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig
+
+
 
 class TestApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         init()
+        setFont()
+    }
+
+    private fun setFont() {
+        CalligraphyConfig.initDefault(
+            CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/HelveticaNeue.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        )
     }
 
     private fun init() {
